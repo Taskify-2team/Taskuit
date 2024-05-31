40665545031b6274d55fbfdf3d7860905e5e79c5
kidboi666 <kidboi666@gmail.com>
2024-05-31 15:56:36 +0900

    style : 스타일 rem 수치 수정

--- components/DropDownInputMenu/DropDownInputMenu.tsx
+++ components/DropDownInputMenu/DropDownInputMenu.tsx
@@ -36,23 +36,23 @@ export default function DropDownInputMenu({ menuList, menuListIcon }: DropDownIn
     <div
       ref={refNode}
       onClick={() => setShowMenuList((prev) => !prev)}
-      className={`${showMenuList ? 'border-violet-500' : 'border-gray-300'} relative cursor-pointer w-full h-12 bg-white px-2 py-6 border-solid border rounded-md text-sm text-black`}
+      className={`${showMenuList ? 'border-violet-500' : 'border-gray-300'} relative cursor-pointer w-full h-[4.8rem] bg-white px-2 py-6 border-solid border rounded-md text-black`}
     >
       <div className="size-full flex justify-between items-center">
-        <div className="col-start-2 flex gap-1 px-2 py-1 text-black">
-          <input value={selectMenu} onChange={changeInput} className="outline-none" />
+        <div className="col-start-2 flex gap-1 px-[0.8rem] py-[0.4rem] text-black">
+          <input value={selectMenu} onChange={changeInput} className="outline-none text-[1.2rem]" />
         </div>
         <div>
           <Image src={arrow} alt="드롭다운 화살표" className={showMenuList ? 'rotate-180' : ''} />
         </div>
       </div>
       {showMenuList && (
-        <div className="absolute left-0 top-[3.2rem] w-full border border-solid border-gray-300 rounded-md flex flex-col overflow-hidden shadow-lg animate-slideDown bg-white">
+        <div className="absolute left-0 top-[5rem] py-[0.65rem] w-full border border-solid border-gray-300 rounded-md flex flex-col overflow-hidden shadow-lg animate-slideDown bg-white">
           {menuList.map((menuItem, i) => (
             <div
               key={menuItem}
               onClick={() => setSelectMenu(menuItem)}
-              className="relative w-full h-12 px-2 py-6 grid grid-cols-[1rem_1fr] gap-1 content-center place-items-start border-solid border-1 border-blue-500 hover:bg-slate-200"
+              className="relative w-full h-full px-[1.6rem] py-[0.65rem] grid grid-cols-[2.2rem_1fr] gap-1 content-center place-items-start border-solid border-1 border-blue-500 hover:bg-slate-200"
             >
               {menuItem === selectMenu && (
                 <div className="col-start-1 self-center">
