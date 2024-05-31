a46b8e8bf4f17c939094e1715e176d98ea0589ee
kidboi666 <kidboi666@gmail.com>
2024-05-31 18:28:55 +0900

    feat : 담당자 지정 기능 약식 구현

--- components/DropDownInputMenu/DropDownInputMenu.tsx
+++ components/DropDownInputMenu/DropDownInputMenu.tsx
@@ -3,26 +3,63 @@
 import { ChangeEvent, useEffect, useRef, useState } from 'react'
 import arrow from '@/public/icons/arrow.svg'
 import check from '@/public/icons/check.svg'
+import cancel from '@/public/icons/cancel.svg'
 import Image from 'next/image'
 
-interface DropDownInputMenuProps {
-  menuList: string[]
-  menuListIcon: string[]
+interface MenuList {
+  id: number
+  nickname: string
+  profileImageUrl: string
 }
 
-export default function DropDownInputMenu({ menuList, menuListIcon }: DropDownInputMenuProps) {
+interface DropDownInputMenuProps {
+  menuList: MenuList[]
+}
+
+export default function DropDownInputMenu({ menuList }: DropDownInputMenuProps) {
   const [selectMenu, setSelectMenu] = useState({
-    name: '',
-    icon: '',
+    id: 0,
+    nickname: '',
+    profileImageUrl: '',
   })
+  const [foundList, setFoundList] = useState<MenuList[]>(menuList)
   const [showMenuList, setShowMenuList] = useState(false)
   const refNode = useRef<HTMLDivElement>(null)
 
-  const changeInput = (e: ChangeEvent<HTMLInputElement>) => {
+  const isKoreanFoundValue = (value: string) => {
+    const koreanPattern = /[가-힣]/
+    return value.match(koreanPattern)
+  }
+
+  const initializeSelectMenu = () => {
     setSelectMenu({
-      ...selectMenu,
-      name: e.target.value,
+      id: 0,
+      nickname: '',
+      profileImageUrl: '',
     })
+    setFoundList(menuList)
+  }
+
+  const findMatchingItemList = (inputValue: string) => {
+    const result = menuList.filter((menuItem) => {
+      return (
+        menuItem.nickname.toLowerCase().includes(inputValue.toLowerCase()) &&
+        isKoreanFoundValue(inputValue)
+      )
+    })
+    setFoundList(result)
+  }
+
+  const changeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    findMatchingItemList(e.target.value)
+    if (e.target.value) {
+      setSelectMenu({
+        ...selectMenu,
+        nickname: e.target.value,
+      })
+    } else {
+      initializeSelectMenu()
+    }
   }
 
   useEffect(() => {
@@ -36,31 +73,40 @@ export default function DropDownInputMenu({ menuList, menuListIcon }: DropDownIn
     return () => {
       document.removeEventListener('mousedown', handleClickOutside)
     }
-  })
+  }, [refNode])
 
   return (
     <div
       ref={refNode}
-      onClick={() => setShowMenuList((prev) => !prev)}
+      onClick={() => setShowMenuList(true)}
       className={`${showMenuList ? 'border-violet-500' : 'border-gray-300'} relative h-[4.8rem] w-full cursor-pointer rounded-md border border-solid bg-white px-[1.6rem] py-[1.3rem] text-black`}
     >
-      <div className="flex size-full items-center justify-between">
-        <div className="col-start-2 flex gap-1 px-[0.8rem] py-[0.4rem] text-black">
-          <div className="relative h-[2.6rem] w-[2.6rem]">
-            {selectMenu.icon && (
-              <Image
-                fill
-                src={selectMenu.icon}
-                alt="프로필 이미지"
-                className="rounded-full border-[0.2rem]"
-              />
-            )}
-          </div>
+      <div className="flex size-full items-center">
+        <div className="grid grid-cols-[1.5rem_1fr] items-center gap-1 text-black">
+          {selectMenu.profileImageUrl && (
+            <>
+              <button
+                type="button"
+                className="relative col-start-1 h-[1.2rem] w-[1.2rem]"
+                onClick={initializeSelectMenu}
+              >
+                <Image fill src={cancel} alt="취소 버튼" />
+              </button>
+              <div className="relative col-start-2 h-[2.6rem] w-[2.6rem]">
+                <Image
+                  fill
+                  src={selectMenu.profileImageUrl}
+                  alt="프로필 이미지"
+                  className="rounded-full border-[0.2rem]"
+                />
+              </div>
+            </>
+          )}
           <input
-            value={selectMenu.name}
+            value={selectMenu.nickname}
             onChange={changeInput}
-            className="text-[1.2rem] outline-none"
-            placeholder="담당자"
+            className="col-start-3 text-[1.2rem] outline-none"
+            placeholder="담당자를 선택해 주세요"
           />
         </div>
         <div>
@@ -69,14 +115,20 @@ export default function DropDownInputMenu({ menuList, menuListIcon }: DropDownIn
       </div>
       {showMenuList && (
         <div className="absolute left-0 top-[5rem] flex w-full animate-slideDown flex-col overflow-hidden rounded-md border border-solid border-gray-300 bg-white py-[0.65rem] shadow-lg">
-          {menuList.map((menuItem, i) => (
+          {foundList.map((menuItem) => (
             <div
-              key={menuItem}
-              onClick={() => setSelectMenu({ name: menuItem, icon: menuListIcon[i] })}
+              key={menuItem.id}
+              onClick={() =>
+                setSelectMenu({
+                  id: menuItem.id,
+                  nickname: menuItem.nickname,
+                  profileImageUrl: menuItem.profileImageUrl,
+                })
+              }
               className="relative grid h-full w-full grid-cols-[1.5rem_1fr] place-items-start content-center gap-1 px-[1.6rem] py-[0.65rem] hover:bg-slate-200"
             >
-              {menuItem === selectMenu.name && (
-                <div className="col-start-1 self-center">
+              {menuItem.nickname === selectMenu.nickname && (
+                <div className="relative col-start-1 self-center">
                   <Image src={check} alt="체크 표시" />
                 </div>
               )}
@@ -84,12 +136,12 @@ export default function DropDownInputMenu({ menuList, menuListIcon }: DropDownIn
                 <div className="relative h-[2.6rem] w-[2.6rem]">
                   <Image
                     fill
-                    src={menuListIcon[i]}
+                    src={menuItem.profileImageUrl}
                     alt="프로필 이미지"
                     className="rounded-full border-[0.2rem]"
                   />
                 </div>
-                <div className="text-[1.2rem]">{menuItem}</div>
+                <div className="text-[1.2rem]">{menuItem.nickname}</div>
               </div>
             </div>
           ))}
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,27 @@ import DropDownMenu from '@/components/DropDownMenu/DropDownMenu'
 export default function Home() {
   const mockUser = [
     {
+      id: 1,
       nickname: '정지성',
       profileImageUrl: 'https://avatars.githubusercontent.com/u/4323180?s=64&v=4',
     },
     {
+      id: 2,
       nickname: '박상준',
       profileImageUrl: 'https://avatars.githubusercontent.com/u/882133?s=64&v=4',
     },
     {
+      id: 3,
       nickname: '오다은',
       profileImageUrl: 'https://avatars.githubusercontent.com/u/589410?s=64&v=4',
     },
     {
+      id: 4,
       nickname: '박희진',
       profileImageUrl: 'https://avatars.githubusercontent.com/u/2250252?s=64&v=4',
     },
     {
+      id: 5,
       nickname: '이진욱',
       profileImageUrl: 'https://avatars.githubusercontent.com/u/2615508?s=64&v=4',
     },
@@ -28,10 +33,7 @@ export default function Home() {
   return (
     <div className="flex gap-4">
       <div className="w-[21.7rem]">
-        <DropDownInputMenu
-          menuList={mockUser.map((v) => v.nickname)}
-          menuListIcon={mockUser.map((v) => v.profileImageUrl)}
-        />
+        <DropDownInputMenu menuList={mockUser} />
       </div>
       <div className="w-[21.7rem]">
         <DropDownMenu menuList={mockUser.map((v) => v.nickname)} />
