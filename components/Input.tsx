53ee87d37d8147bbbb33514cfb0f5b97b70b52ec
daeun <qzsz797979@gmail.com>
2024-05-31 12:05:56 +0900

    feat: Input 컴포넌트 생성

---
+++ components/Input.tsx
@@ -0,0 +1,10 @@
+export default function Input() {
+  return (
+    <div className="">
+      <label htmlFor="id" className="">
+        이메일
+      </label>
+      <input id="id" type="text" />
+    </div>
+  );
+}
