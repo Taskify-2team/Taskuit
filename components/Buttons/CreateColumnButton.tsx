4d43d2915eeb73b8f440152ebdfceb8bdbb891b7
sj0724 <sj07245@naver.com>
2024-05-31 22:08:51 +0900

    fix: lint 컴포넌트 네이밍 컨벤션 규칙 추가, onclick 규칙 off, prettier-plugin-tailwind 설치

--- components/Buttons/CreateColumnButton.tsx
+++ components/Buttons/CreateColumnButton.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image'
 
 export default function CreateColumnButton() {
   return (
-    <div className="lg:w-[35.4rem] w-[54.4rem] h-[7rem] flex items-center gap-[1.2rem] justify-center border border-solid rounded-[0.6rem] cursor-pointer">
-      <p className="text-[1.8rem] text-nowrap font-bold">새로운 컬럼 추가하기</p>
-      <div className="w-[2.2rem] h-[2.2rem] bg-[--violet-violet-8] rounded-[0.4rem] relative p-[0.3rem] border-[--gray-gray_D9D9D9]">
+    <div className="flex h-[7rem] w-[54.4rem] cursor-pointer items-center justify-center gap-[1.2rem] rounded-[0.6rem] border border-solid lg:w-[35.4rem]">
+      <p className="text-nowrap text-[1.8rem] font-bold">새로운 컬럼 추가하기</p>
+      <div className="relative h-[2.2rem] w-[2.2rem] rounded-[0.4rem] border-[--gray-gray_D9D9D9] bg-[--violet-violet-8] p-[0.3rem]">
         <Image src="/icons/addLogo.svg" alt="더하기 이미지" fill />
       </div>
     </div>
--- components/Buttons/ShortButtons/WhiteButton.tsx
+++ components/Buttons/ShortButtons/WhiteButton.tsx
@@ -1,6 +1,6 @@
 export default function WhiteButton() {
   return (
-    <span className="inline-block w-[8.4rem] py-[0.7rem] text-center bg-[--white-white_FFFFFF] rounded-[0.4rem] text-[--violet-violet_5534DA] text-[1.4rem] cursor-pointer leading-tight border border-solid border-[--gray-gray_D9D9D9]">
+    <span className="inline-block w-[8.4rem] cursor-pointer rounded-[0.4rem] border border-solid border-[--gray-gray_D9D9D9] bg-[--white-white_FFFFFF] py-[0.7rem] text-center text-[1.4rem] leading-tight text-[--violet-violet_5534DA]">
       버튼
     </span>
   )
