74b3fbe9e954af4e80596258b8020e302fae66f1
kidboi666 <kidboi666@gmail.com>
2024-05-31 14:46:27 +0900

    fix : lint a11y 주석 처리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import DropDownMenu from '@/components/DropDownMenu/DropDownMenu'
+
 export default function Home() {
-  return <div className="w-[10rem]">프로젝트</div>
+  const mock = ['On Progress', 'To Do', 'Success']
+  return (
+    <div className="w-[21.7rem]">
+      <DropDownMenu menuList={mock} />
+    </div>
+  )
 }
