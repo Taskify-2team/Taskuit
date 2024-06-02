62eb67578094c7abffc6c63eec33e6b69d904ff6
daeun <qzsz797979@gmail.com>
2024-06-02 16:30:57 +0900

    feat: Textarea 컴포넌트 구현

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,5 +1,6 @@
 import AuthInput from '@/components/Input/AuthInput'
 import TextInput from '@/components/Input/TextInput'
+import Textarea from '@/components/Input/Textarea'
 
 export default function test() {
   const error = {
@@ -9,6 +10,7 @@ export default function test() {
   return (
     <div>
       <TextInput id="0" label="dddd" placeholder="dfsdf" />
+      <Textarea id="0" label="dddd" placeholder="dfsdf" />
       <AuthInput id="1" label="dd" placeholder="111" type="email" />
       <AuthInput id="2" label="dd" placeholder="111" type="password" />
       <AuthInput id="3" label="dd" placeholder="111" type="email" error={error} />
