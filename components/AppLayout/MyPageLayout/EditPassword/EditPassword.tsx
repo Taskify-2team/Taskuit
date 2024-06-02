0592c8a7d7fb282f108735aef2ff89efc94c8b4b
daeun <qzsz797979@gmail.com>
2024-06-02 11:35:10 +0900

    feat: EditPassword 컴포넌트에 TextInput 컴포넌트 적용

--- components/AppLayout/MyPageLayout/EditPassword/EditPassword.tsx
+++ components/AppLayout/MyPageLayout/EditPassword/EditPassword.tsx
@@ -1,37 +1,14 @@
 import { PurpleButton } from '@/components'
+import TextInput from '@/components/Input/TextInput'
 
 export default function EditPassword() {
   return (
     <div className="flex w-[62rem] flex-col rounded-[0.8rem] bg-var-white p-[2.8rem]">
       <h3 className="mb-[3.2rem] text-[2.4rem] font-bold">비밀번호 변경</h3>
       <div className="mb-[2.4rem] flex flex-col gap-[2rem]">
-        <label htmlFor="currentPassword" className="flex flex-col gap-[1rem] text-[1.8rem]">
-          현재 비밀번호
-          <input
-            id="currentPassword"
-            placeholder="현재 비밀번호 입력"
-            className="rounded-[0.6rem] border-[0.1rem] border-var-gray3 pb-[1.2rem] pl-[1.6rem] pt-[1.3rem] text-[1.6rem]"
-          />
-          {/** 인풋 컴포넌트 교체 예정 */}
-        </label>
-        <label htmlFor="nextPassword" className="flex flex-col gap-[1rem] text-[1.8rem]">
-          새 비밀번호
-          <input
-            id="nextPassword"
-            placeholder="새 비밀번호 입력"
-            className="rounded-[0.6rem] border-[0.1rem] border-var-gray3 pb-[1.2rem] pl-[1.6rem] pt-[1.3rem] text-[1.6rem]"
-          />
-          {/** 인풋 컴포넌트 교체 예정 */}
-        </label>
-        <label htmlFor="checkNextPassword" className="flex flex-col gap-[1rem] text-[1.8rem]">
-          새 비밀번호 확인
-          <input
-            id="checkNextPassword"
-            placeholder="새 비밀번호 입력"
-            className="rounded-[0.6rem] border-[0.1rem] border-var-gray3 pb-[1.2rem] pl-[1.6rem] pt-[1.3rem] text-[1.6rem]"
-          />
-          {/** 인풋 컴포넌트 교체 예정 */}
-        </label>
+        <TextInput id="currentPassword" label="현재 비밀번호" placeholder="현재 비밀번호 입력" />
+        <TextInput id="nextPassword" label="새 비밀번호" placeholder="새 비밀번호 입력" />
+        <TextInput id="checkNextPassword" label="새 비밀번호 확인" placeholder="새 비밀번호 입력" />
       </div>
       <div className="self-end">
         <PurpleButton text="변경" />
--- pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import TextInput from '@/components/Input/TextInput'
-
-export default function test() {
-  return (
-    <div>
-      <TextInput label="제목" placeholder="제목을 입력해 주세요." isRequired />
-    </div>
-  )
-}
