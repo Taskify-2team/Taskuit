c8bc4e4a3b20fe32650dc2d53543252e22636baa
daeun <qzsz797979@gmail.com>
2024-06-02 17:01:32 +0900

    design: input padding 조정

--- components/Input/inputstyles.ts
+++ components/Input/inputstyles.ts
@@ -1,2 +1,2 @@
 export const inputStyles =
-  'rounded-[0.6rem] border-[0.1rem] border-var-gray3 px-[1.6rem] py-[1.5rem] text-[1.6rem] outline-none focus:border-primary-violet'
+  'rounded-[0.6rem] border-[0.1rem] border-var-gray3 px-[1.6rem] py-[1.2rem] text-[1.6rem] outline-none focus:border-primary-violet'
