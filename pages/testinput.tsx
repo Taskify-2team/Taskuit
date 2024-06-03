58adab88e34606b5ab0b658dcd370de8f6962202
daeun <qzsz797979@gmail.com>
2024-06-03 20:52:31 +0900

    test: test페이지 삭제

--- pages/testinput.tsx
+++
@@ -1,9 +0,0 @@
-import DateInput from '@/components/Input/DateInput'
-
-export default function testinput() {
-  return (
-    <div>
-      <DateInput id="0" label="마감일" placeholder="날짜를 입력해 주세요" />
-    </div>
-  )
-}
