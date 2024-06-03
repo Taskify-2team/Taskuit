175d8e3fe61f41923fde8c953573039d797cb0f0
daeun <qzsz797979@gmail.com>
2024-06-03 10:54:44 +0900

    chore: @types/react-datepicker 설치

--- components/Input/DateInput.tsx
+++ components/Input/DateInput.tsx
@@ -1,6 +1,8 @@
+import DatePicker from 'react-datepicker'
 import InputLayout from './InputLayout'
 import { inputStyles } from './inputstyles'
 import Calendar from '@/public/icons/calendar.svg'
+import { useState } from 'react'
 
 interface DateInputProps {
   id: string
@@ -10,9 +12,6 @@ interface DateInputProps {
 }
 
 export default function DateInput({ id, label, placeholder, isRequired }: DateInputProps) {
-  return (
-    <InputLayout id={id} label={label} isRequired={isRequired}>
-      <input id={id} placeholder={placeholder} required={isRequired} className={inputStyles} />
-    </InputLayout>
-  )
+  const [startDate, setStartDate] = useState(new Date())
+  return <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
 }
