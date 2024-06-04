3d4cb2a6f8529d391e7571ba348139ffbd8eb363
Jisung Jeong <ssjeond@naver.com>
2024-06-04 18:05:19 +0900

    fix: 리액트 훅 폼 린트 수정

--- components/Input/AuthInput.tsx
+++ components/Input/AuthInput.tsx
@@ -10,6 +10,7 @@ interface AuthInputProps {
   placeholder: string
   type: string
   label: string
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   field: ControllerRenderProps<FormValueType, any>
   error: FieldError | undefined
 }
