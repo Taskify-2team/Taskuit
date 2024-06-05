a7b061e926da449bb540c23353f207bd60420f61
sj0724 <sj07245@naver.com>
2024-06-05 23:34:42 +0900

    feat: 이름 편집 컴포넌트 분리, 멤버 수정 컴포넌트 구현

--- components/UserInfo/UserInfo.tsx
+++ components/UserInfo/UserInfo.tsx
@@ -7,7 +7,7 @@ export interface UserInfoProps {
 
 export default function UserInfo({ profileImageUrl, nickname }: UserInfoProps) {
   return (
-    <div className="flex items-center gap-5 pl-12 pr-32">
+    <div className="flex items-center gap-5">
       <UserProfile profileImageUrl={profileImageUrl} nickname={nickname} />
       <p className="text-2xl">{nickname}</p>
     </div>
--- service/members.ts
+++ service/members.ts
@@ -1,7 +1,9 @@
 import axios from './instance'
 
-export const getMemberList = async (params) => {
-  const query = `?page=${params.page || 1}&size=${params.size || 20}&dashboardId=${params.dashboardId}`
+const getMemberList = async (page: number, id: number) => {
+  const query = `?page=${page || 1}&size=4&dashboardId=${id}`
   const response = await axios.get(`/members${query}`)
-  return response
+  return response.data
 }
+
+export default getMemberList
