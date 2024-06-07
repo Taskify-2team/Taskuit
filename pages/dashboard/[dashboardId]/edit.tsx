579656ebd8bfdd496af9f58f4c45e9b85efc063c
sj0724 <sj07245@naver.com>
2024-06-07 12:45:40 +0900

    feat: 뒤로가기 버튼 구현

--- pages/dashboard/[dashboardId]/edit.tsx
+++ pages/dashboard/[dashboardId]/edit.tsx
@@ -4,6 +4,8 @@ import EditMember from '@/components/Edit/EditMember'
 import EditName from '@/components/Edit/EditName'
 import { deleteDashBoard } from '@/service/dashboards'
 import { useRouter } from 'next/router'
+import backIcon from '@/public/icons/arrowBack.svg'
+import Image from 'next/image'
 
 export default function Edit() {
   const router = useRouter()
@@ -18,7 +20,11 @@ export default function Edit() {
 
   return (
     <AppLayout>
-      <div className="flex flex-col items-center justify-center gap-[1.2rem]">
+      <div className="flex flex-col gap-[1.2rem]">
+        <div className="flex cursor-pointer gap-[0.6rem] py-[1.3rem]" onClick={() => router.back()}>
+          <Image src={backIcon} alt="화살표" width={20} height={20} />
+          <p className="text-[1.6rem]">돌아가기</p>
+        </div>
         <EditName />
         <EditMember />
         <EditInvitation />
