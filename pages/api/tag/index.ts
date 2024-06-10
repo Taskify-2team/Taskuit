dde1f9a69346d68f328357bdfe8f15d45fdd29eb
kidboi666 <kidboi666@gmail.com>
2024-06-10 11:29:25 +0900

    chore : 몽구스 설치

--- pages/api/tag/index.ts
+++ pages/api/tag/index.ts
@@ -6,26 +6,17 @@ import { NextApiRequest, NextApiResponse } from 'next'
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   cors(req, res, async () => {
     await dbConnect()
-    const { cardId } = req.query
 
     switch (req.method) {
-      case 'PATCH': {
-        const updateTag = await Tag.findByIdAndUpdate(cardId, req.body, {
-          new: true,
-        })
-        res.status(200).send(updateTag)
+      case 'POST': {
+        const newTag = await Tag.create(req.body)
+        res.status(200).send(newTag)
         break
       }
 
       case 'GET': {
-        const foundTag = await Tag.findById(cardId)
-        res.status(200).send(foundTag)
-        break
-      }
-
-      case 'DELETE': {
-        await Tag.findByIdAndDelete(cardId)
-        res.status(204).end()
+        const foundTag = await Tag.find()
+        res.status(200).send({ data: foundTag })
         break
       }
 
