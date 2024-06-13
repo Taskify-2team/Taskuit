2922ca2546d7238eee0c6aeb4effb7d9e1c92fee
sj0724 <sj07245@naver.com>
2024-06-13 19:07:40 +0900

    fix: 가로 스크롤 구현 완료, 마우스로 동작 가능

--- pages/dashboard/[dashboardId]/index.tsx
+++ pages/dashboard/[dashboardId]/index.tsx
@@ -2,18 +2,51 @@ import { AppLayout, DashboardLayout } from '@/components'
 import { useAppDispatch } from '@/hooks/useApp'
 import { getColumnList } from '@/service/columns'
 import { useRouter } from 'next/router'
-import { useCallback, useEffect } from 'react'
+import { MouseEvent, useCallback, useEffect, useRef, useState } from 'react'
 
 export default function Dashboard() {
+  const [isDrag, setIsDrag] = useState(false)
+  const [totalX, setTotalX] = useState(0)
   const dispatch = useAppDispatch()
   const router = useRouter()
   const { dashboardId } = router.query
+  const scrollRef = useRef<HTMLDivElement>(null)
+
+  const preventUnexpectedEffects = useCallback((e: MouseEvent) => {
+    e.stopPropagation()
+  }, [])
 
   const getColumnsData = useCallback(async () => {
     if (typeof dashboardId === 'string') {
       await dispatch(getColumnList(dashboardId))
     }
   }, [dashboardId, dispatch])
+
+  const onAxisDragStart = (e: MouseEvent) => {
+    preventUnexpectedEffects(e)
+    setIsDrag(true)
+    const xAxis = e.clientX
+    if (scrollRef.current && 'scrollLeft' in scrollRef.current) {
+      setTotalX(xAxis + scrollRef.current.scrollLeft)
+    }
+  }
+
+  const onAxisDragMove = (e: MouseEvent) => {
+    preventUnexpectedEffects(e)
+    if (!isDrag) {
+      return
+    }
+    const scrollLeft = totalX - e.clientX
+    if (scrollRef.current && 'scrollLeft' in scrollRef.current) {
+      scrollRef.current.scrollLeft = scrollLeft
+    }
+  }
+
+  const onAxisDragEnd = () => {
+    if (!isDrag) return
+    if (!scrollRef.current) return
+    setIsDrag(false)
+  }
 
   useEffect(() => {
     if (dashboardId) {
@@ -23,8 +56,15 @@ export default function Dashboard() {
 
   return (
     <AppLayout>
-      <div className="flex h-full w-full overflow-auto">
-        <DashboardLayout dashboardId={Number(dashboardId)} />
+      <div
+        className="flex h-full w-full overflow-auto"
+        ref={scrollRef}
+        onMouseDown={onAxisDragStart}
+        onMouseMove={onAxisDragMove}
+        onMouseUp={onAxisDragEnd}
+        onMouseLeave={onAxisDragEnd}
+      >
+        <DashboardLayout dashboardId={Number(dashboardId)} setIsDrag={setIsDrag} />
       </div>
     </AppLayout>
   )
