a7c2eab3c22296ebda07ab5ae02b24f7379dfb73
daeun <qzsz797979@gmail.com>
2024-06-13 15:09:24 +0900

    docs: head title 수정, 기본 언어값 ko로 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head>
         <script defer src="https://developers.kakao.com/sdk/js/kakao.min.js" />
       </Head>
