b908d21b280f4802c68409859e5f595f9493d96a
daeun <qzsz797979@gmail.com>
2024-06-14 13:27:10 +0900

    design: dashboard header 반응형 수정

--- components/Headers/HeaderDropDown.tsx
+++ components/Headers/HeaderDropDown.tsx
@@ -14,25 +14,27 @@ function Dropdown({ theme }: DropdownProps) {
 
   return (
     <div
-      className={`absolute left-[1rem] top-[4rem] flex w-[11rem] animate-slideDown flex-col overflow-hidden rounded-md border border-solid text-center sm:w-[9rem] md:left-[0.5rem] ${
+      className={`absolute left-[1rem] top-[4.5rem] flex w-[11rem] animate-slideDown flex-col overflow-hidden rounded-md border border-solid p-[0.5rem] text-center sm:w-[9rem] md:left-[0.5rem] ${
         theme === 'normal'
           ? 'border-var-gray3 bg-var-white'
           : 'border-var-black1 bg-var-black1 text-white'
       } shadow-lg`}
     >
-      <Link href="/mypage">
-        <p className="block w-full px-4 py-2 text-left text-[1.6rem] hover:bg-var-gray4 sm:text-[1.3rem]">
-          내 정보
-        </p>
+      <Link
+        href="/mypage"
+        className="block w-full rounded-[0.4rem] px-4 py-[1rem] text-left text-[1.6rem] hover:bg-var-violet sm:text-[1.3rem]"
+      >
+        내 정보
       </Link>
-      <Link href="/mydashboard">
-        <p className="block w-full px-4 py-2 text-left text-[1.6rem] hover:bg-var-gray4 sm:text-[1.3rem]">
-          내 대시보드
-        </p>
+      <Link
+        href="/mydashboard"
+        className="block w-full rounded-[0.4rem] px-4 py-[1rem] text-left text-[1.6rem] hover:bg-var-violet sm:text-[1.3rem]"
+      >
+        내 대시보드
       </Link>
       <button
         type="button"
-        className="border-t- block w-full px-4 py-2 text-left text-[1.6rem] hover:bg-var-gray4 sm:text-[1.3rem]"
+        className="border-t-block w-full rounded-[0.4rem] px-4 py-[1rem] text-left text-[1.6rem] hover:bg-var-violet sm:text-[1.3rem]"
         onClick={handleLogout}
       >
         로그아웃
