107df9ebed24cce8420361aad1d8b8768092095e
kidboi666 <kidboi666@gmail.com>
2024-06-14 16:17:24 +0900

    feat : 구현완료 이제 컨플릭트 해결만 남음

--- components/AppLayout/DashboardLayout/components/DashBoardColumn.tsx
+++ components/AppLayout/DashboardLayout/components/DashBoardColumn.tsx
@@ -47,7 +47,7 @@ export default function DashBoardColumn({
 
       setTagList(result)
     }
-  }, [getTagListRequest])
+  }, [getTagListRequest, dbId])
 
   const handleObserver = useCallback(
     (entries: IntersectionObserverEntry[]) => {
