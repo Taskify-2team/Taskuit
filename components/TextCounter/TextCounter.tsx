b557ad397122367f856a380751e850e287110b40
sj0724 <sj07245@naver.com>
2024-06-14 15:59:42 +0900

    style: 텍스트카운터 다크모드 적용

--- components/TextCounter/TextCounter.tsx
+++ components/TextCounter/TextCounter.tsx
@@ -1,13 +1,25 @@
+import { useLoadTheme } from '@/store/context/ThemeContext'
+import { useEffect, useState } from 'react'
+
 interface TextCounterProps {
   text: string
   length: number
 }
 
 export default function TextCounter({ text, length }: TextCounterProps) {
+  const { theme } = useLoadTheme()
+  const [textColor, setTextColor] = useState('text-var-black4')
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      setTextColor('text-var-gray3')
+    }
+  }, [theme])
+
   return (
     <div className="absolute bottom-[-2rem] right-0">
       <p
-        className={`${text.length === length ? 'text-var-red' : 'text-var-black4'} text-[1.2rem]`}
+        className={`${text.length === length ? 'text-var-red' : textColor} text-[1.2rem]`}
       >{`${text.length}/${length}`}</p>
     </div>
   )
