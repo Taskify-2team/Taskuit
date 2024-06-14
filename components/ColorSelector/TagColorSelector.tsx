9d50eeb322ed21ebbbb60182788f502763dd7fec
kidboi666 <kidboi666@gmail.com>
2024-06-14 16:31:07 +0900

    fix : tagInput 깃 충돌 해결

--- components/ColorSelector/TagColorSelector.tsx
+++ components/ColorSelector/TagColorSelector.tsx
@@ -38,6 +38,7 @@ export default function TagColorSelector({ idx, onClose, setMyTagBody }: TagColo
       document.removeEventListener('mousedown', handleOutsideClick)
     }
   }, [])
+
   return (
     <ul
       ref={colorPickerRef}
