6edadc619452c7eac03d1f210350b309ec66476a
daeun <qzsz797979@gmail.com>
2024-06-15 22:25:57 +0900

    design: 랜딩페이지 영어버전 반응형 ui 수정

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { useLoadTheme } from '@/store/context/ThemeContext'
 
 export default function Document() {
+  const { theme } = useLoadTheme()
+
   return (
     <Html lang="ko">
       <Head>
         <script defer src="https://developers.kakao.com/sdk/js/kakao.min.js" />
       </Head>
-      <body>
+      <body className={theme === 'normal' ? '' : 'bg-var-black2'}>
         <Main />
         <NextScript />
         <div id="modal" />
