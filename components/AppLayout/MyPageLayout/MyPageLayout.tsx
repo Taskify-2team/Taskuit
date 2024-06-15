a577f13e945c58cea0150dcfd5d4ca82a92f8af6
kidboi666 <kidboi666@gmail.com>
2024-06-15 14:00:24 +0900

    feat : mypage 돌아가기 버튼 추가

--- components/AppLayout/MyPageLayout/MyPageLayout.tsx
+++ components/AppLayout/MyPageLayout/MyPageLayout.tsx
@@ -1,3 +1,8 @@
+import { useLoadTheme } from '@/store/context/ThemeContext'
+import Image from 'next/image'
+import backIcon from '@/public/icons/arrowBack.svg'
+import backIconGray from '@/public/icons/arrowBackGrey.svg'
+import { useRouter } from 'next/router'
 import { ReactNode } from 'react'
 
 interface MyPageLayoutProps {
@@ -6,8 +11,24 @@ interface MyPageLayoutProps {
 }
 
 export default function MyPageLayout({ EditProfile, EditPassword }: MyPageLayoutProps) {
+  const { theme } = useLoadTheme()
+  const router = useRouter()
   return (
     <div className="flex flex-col gap-[1.2rem] p-[2rem] sm:p-[1.2rem]">
+      <div
+        className="my-[1.3rem] flex w-fit cursor-pointer gap-[0.6rem]"
+        onClick={() => router.back()}
+      >
+        <Image
+          src={theme === 'normal' ? backIcon : backIconGray}
+          alt="화살표"
+          width={20}
+          height={20}
+        />
+        <p className={`text-[1.6rem] ${theme === 'normal' ? 'text-var-black4' : 'text-var-gray3'}`}>
+          돌아가기
+        </p>
+      </div>
       {EditProfile}
       {EditPassword}
     </div>
