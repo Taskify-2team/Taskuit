1e3e1f56d7b3ce3112d578e243ff42b3b997be86
kidboi666 <kidboi666@gmail.com>
2024-06-16 16:33:41 +0900

    feat : 이미지 로딩화면 구현

---
+++ components/Loading/LoadingBox.tsx
@@ -0,0 +1,9 @@
+export default function LoadingBox({ theme }: { theme: string }) {
+  return (
+    <div className="mb-[1.2rem] flex animate-pulse gap-[0.6rem]">
+      <div
+        className={`h-[26rem] w-full rounded-[0.6rem] ${theme === 'normal' ? 'border-var-gray2 bg-var-gray2' : 'border-var-black1 bg-var-black1'}`}
+      />
+    </div>
+  )
+}
